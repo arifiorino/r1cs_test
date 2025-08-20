8be8e34775167cf17b8aaf1473f19bbabfda4be2
Ari Fiorino <arifiorino@gmail.com>
2025-08-20 16:28:15 -0500

    Initial sample circuit

---
+++ src/main.rs
@@ -0,0 +1,125 @@
+use ark_ff::Field;
+use ark_relations::{
+  lc,
+  r1cs::{ConstraintSynthesizer, ConstraintSystemRef, SynthesisError},
+};
+use ark_std::marker::PhantomData;
+
+#[derive(Copy, Clone)]
+struct Circuit<F: Field> {
+  a: Option<F>,
+  b: Option<F>,
+  num_constraints: usize,
+  num_variables: usize,
+}
+
+impl<ConstraintF: Field> ConstraintSynthesizer<ConstraintF> for Circuit<ConstraintF> {
+  fn generate_constraints(
+    self,
+    cs: ConstraintSystemRef<ConstraintF>,
+  ) -> Result<(), SynthesisError> {
+    let a = cs.new_witness_variable(|| self.a.ok_or(SynthesisError::AssignmentMissing))?;
+    let b = cs.new_witness_variable(|| self.b.ok_or(SynthesisError::AssignmentMissing))?;
+    let c = cs.new_input_variable(|| {
+      let mut a = self.a.ok_or(SynthesisError::AssignmentMissing)?;
+      let b = self.b.ok_or(SynthesisError::AssignmentMissing)?;
+
+      a.mul_assign(&b);
+      Ok(a)
+    })?;
+    let d = cs.new_input_variable(|| {
+      let mut a = self.a.ok_or(SynthesisError::AssignmentMissing)?;
+      let b = self.b.ok_or(SynthesisError::AssignmentMissing)?;
+
+      a.mul_assign(&b);
+      a.mul_assign(&b);
+      Ok(a)
+    })?;
+
+    for _ in 0..(self.num_variables - 3) {
+      let _ = cs.new_witness_variable(|| self.a.ok_or(SynthesisError::AssignmentMissing))?;
+    }
+
+    for _ in 0..(self.num_constraints - 1) {
+      cs.enforce_constraint(lc!() + a, lc!() + b, lc!() + c)?;
+    }
+    cs.enforce_constraint(lc!() + c, lc!() + b, lc!() + d)?;
+
+    Ok(())
+  }
+}
+
+use ark_marlin::Marlin;
+use ark_marlin::rng::FiatShamirRng;
+
+use ark_bls12_381::{Bls12_381, Fr};
+use ark_ff::UniformRand;
+use ark_poly::univariate::DensePolynomial;
+use ark_poly_commit::marlin_pc::MarlinKZG10;
+use ark_std::ops::MulAssign;
+use blake2::Blake2s;
+use rand_chacha::ChaChaRng;
+
+type MultiPC = MarlinKZG10<Bls12_381, DensePolynomial<Fr>>;
+//type FS = FiatShamirRng<Blake2s>;
+type MarlinInst = Marlin<Fr, MultiPC, Blake2s>;
+
+fn test_circuit(num_constraints: usize, num_variables: usize) {
+  let rng = &mut ark_std::test_rng();
+
+  let universal_srs = MarlinInst::universal_setup(100, 25, 300, rng).unwrap();
+
+  for _ in 0..100 {
+    let a = Fr::rand(rng);
+    let b = Fr::rand(rng);
+    let mut c = a;
+    c.mul_assign(&b);
+    let mut d = c;
+    d.mul_assign(&b);
+
+    let circ = Circuit {
+      a: Some(a),
+      b: Some(b),
+      num_constraints,
+      num_variables,
+    };
+
+    let (index_pk, index_vk) = MarlinInst::index(&universal_srs, circ.clone()).unwrap();
+    println!("Called index");
+
+    let proof = MarlinInst::prove(&index_pk, circ, rng).unwrap();
+    println!("Called prover");
+
+    assert!(MarlinInst::verify(&index_vk, &[c, d], &proof, rng).unwrap());
+    println!("Called verifier");
+    println!("\nShould not verify (i.e. verifier messages should print below):");
+    assert!(!MarlinInst::verify(&index_vk, &[a, a], &proof, rng).unwrap());
+  }
+}
+
+fn main(){
+  let num_constraints = 100;
+  let num_variables = 25;
+
+  test_circuit(num_constraints, num_variables);
+
+  let num_constraints = 26;
+  let num_variables = 25;
+
+  test_circuit(num_constraints, num_variables);
+
+  let num_constraints = 25;
+  let num_variables = 100;
+
+  test_circuit(num_constraints, num_variables);
+
+  let num_constraints = 25;
+  let num_variables = 26;
+
+  test_circuit(num_constraints, num_variables);
+
+  let num_constraints = 25;
+  let num_variables = 25;
+
+  test_circuit(num_constraints, num_variables);
+}
