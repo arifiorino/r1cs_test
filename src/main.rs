f92556fedb56e59449c910e40cbd7a17bcb68b22
Ari Fiorino <arifiorino@gmail.com>
2025-08-20 19:56:22 -0500

    Finished matrix multiplication

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
+use ark_marlin::Marlin;
+use ark_std::One;
+use ark_marlin::rng::FiatShamirRng;
+use ark_bn254::{Bn254, Fr};
+use ark_ff::UniformRand;
+use ark_poly::univariate::DensePolynomial;
+use ark_poly_commit::marlin_pc::MarlinKZG10;
+use ark_std::ops::MulAssign;
+use blake2::Blake2s;
+use rand_chacha::ChaChaRng;
 use ark_ff::Field;
 use ark_relations::{
   lc,
   r1cs::{ConstraintSynthesizer, ConstraintSystemRef, SynthesisError},
 };
 use ark_std::marker::PhantomData;
+use rand::Rng;
+use std::ops::AddAssign;
+use ark_ff::PrimeField;
+use rayon::prelude::*;
 
-#[derive(Copy, Clone)]
+#[derive(Clone)]
 struct Circuit<F: Field> {
-  a: Option<F>,
-  b: Option<F>,
-  num_constraints: usize,
-  num_variables: usize,
+  N: usize,
+  A: Vec<Option<F>>,
+  B: Vec<Option<F>>,
+  C: Vec<Option<F>>,
 }
 
 impl<ConstraintF: Field> ConstraintSynthesizer<ConstraintF> for Circuit<ConstraintF> {
@@ -18,108 +32,92 @@ impl<ConstraintF: Field> ConstraintSynthesizer<ConstraintF> for Circuit<Constrai
     self,
     cs: ConstraintSystemRef<ConstraintF>,
   ) -> Result<(), SynthesisError> {
-    let a = cs.new_witness_variable(|| self.a.ok_or(SynthesisError::AssignmentMissing))?;
-    let b = cs.new_witness_variable(|| self.b.ok_or(SynthesisError::AssignmentMissing))?;
-    let c = cs.new_input_variable(|| {
-      let mut a = self.a.ok_or(SynthesisError::AssignmentMissing)?;
-      let b = self.b.ok_or(SynthesisError::AssignmentMissing)?;
-
-      a.mul_assign(&b);
-      Ok(a)
-    })?;
-    let d = cs.new_input_variable(|| {
-      let mut a = self.a.ok_or(SynthesisError::AssignmentMissing)?;
-      let b = self.b.ok_or(SynthesisError::AssignmentMissing)?;
-
-      a.mul_assign(&b);
-      a.mul_assign(&b);
-      Ok(a)
-    })?;
-
-    for _ in 0..(self.num_variables - 3) {
-      let _ = cs.new_witness_variable(|| self.a.ok_or(SynthesisError::AssignmentMissing))?;
+    assert!(self.A.len() == self.N * self.N);
+    assert!(self.B.len() == self.N * self.N);
+    let one = cs.new_input_variable(||Ok(ConstraintF::one())).unwrap();
+    let A:Vec<_> = self.A.iter().map(|x| cs.new_witness_variable(|| x.ok_or(SynthesisError::AssignmentMissing)).unwrap()).collect();
+    let B:Vec<_> = self.B.iter().map(|x| cs.new_witness_variable(|| x.ok_or(SynthesisError::AssignmentMissing)).unwrap()).collect();
+    let C:Vec<_> = self.C.iter().map(|x| cs.new_witness_variable(|| x.ok_or(SynthesisError::AssignmentMissing)).unwrap()).collect();
+    let mults:Vec<_> = (0..self.N * self.N * self.N).map(|idx|{
+      let i = idx / self.N / self.N;
+      let j = idx / self.N % self.N;
+      let k = idx % self.N;
+      cs.new_witness_variable(|| {
+        let mut x = self.A[i * self.N + k].unwrap();
+        let y = self.B[j * self.N + k].unwrap();
+        x.mul_assign(y);
+        Ok(x)
+      }).unwrap()
+    }).collect();
+    for idx in 0..self.N * self.N * self.N{
+      let i = idx / self.N / self.N;
+      let j = idx / self.N % self.N;
+      let k = idx % self.N;
+      cs.enforce_constraint(lc!() + A[i * self.N + k], lc!() + B[j * self.N + k], lc!() + mults[idx]);
     }
-
-    for _ in 0..(self.num_constraints - 1) {
-      cs.enforce_constraint(lc!() + a, lc!() + b, lc!() + c)?;
+    for idx in 0..self.N * self.N{
+      let i = idx / self.N;
+      let j = idx % self.N;
+      let mut lc = lc!();
+      for k in 0..self.N{
+        lc = lc + mults[i * self.N * self.N + j * self.N + k];
+      }
+      cs.enforce_constraint(lc, lc!() + one, lc!() + C[idx]);
     }
-    cs.enforce_constraint(lc!() + c, lc!() + b, lc!() + d)?;
 
     Ok(())
   }
 }
 
-use ark_marlin::Marlin;
-use ark_marlin::rng::FiatShamirRng;
-
-use ark_bls12_381::{Bls12_381, Fr};
-use ark_ff::UniformRand;
-use ark_poly::univariate::DensePolynomial;
-use ark_poly_commit::marlin_pc::MarlinKZG10;
-use ark_std::ops::MulAssign;
-use blake2::Blake2s;
-use rand_chacha::ChaChaRng;
-
-type MultiPC = MarlinKZG10<Bls12_381, DensePolynomial<Fr>>;
-//type FS = FiatShamirRng<Blake2s>;
+type MultiPC = MarlinKZG10<Bn254, DensePolynomial<Fr>>;
 type MarlinInst = Marlin<Fr, MultiPC, Blake2s>;
 
-fn test_circuit(num_constraints: usize, num_variables: usize) {
+fn main(){
   let rng = &mut ark_std::test_rng();
 
-  let universal_srs = MarlinInst::universal_setup(100, 25, 300, rng).unwrap();
+  let N = 16;
 
-  for _ in 0..100 {
-    let a = Fr::rand(rng);
-    let b = Fr::rand(rng);
-    let mut c = a;
-    c.mul_assign(&b);
-    let mut d = c;
-    d.mul_assign(&b);
+  let universal_srs = MarlinInst::universal_setup(N * N * N + N * N, N * N * 3 + N * N * N, 4 * N * N * N, rng).unwrap();
 
-    let circ = Circuit {
-      a: Some(a),
-      b: Some(b),
-      num_constraints,
-      num_variables,
-    };
 
-    let (index_pk, index_vk) = MarlinInst::index(&universal_srs, circ.clone()).unwrap();
-    println!("Called index");
+  let A:Vec<_> = (0..N * N).into_par_iter().map_init(rand::thread_rng, |rng, _| Some(Fr::from(rng.gen_range(-100..100)))).collect();
+  let B:Vec<_> = (0..N * N).into_par_iter().map_init(rand::thread_rng, |rng, _| Some(Fr::from(rng.gen_range(-100..100)))).collect();
+  let C:Vec<_> = (0..N * N).into_par_iter().map(|idx|{
+    let i = idx / N;
+    let j = idx % N;
+    let mut x = A[i * N].unwrap() * B[j * N].unwrap();
+    for k in 1..N{
+      let y = A[i * N + k].unwrap() * B[j * N + k].unwrap();
+      x.add_assign(y);
+    }
+    Some(x)
+  }).collect();
+  //println!("A:{:?}",A.iter().map(|x|x.unwrap().into_repr()).collect::<Vec<_>>());
+  //println!("B:{:?}",B.iter().map(|x|x.unwrap().into_repr()).collect::<Vec<_>>());
+  //println!("C:{:?}",C.iter().map(|x|x.into_repr()).collect::<Vec<_>>());
 
-    let proof = MarlinInst::prove(&index_pk, circ, rng).unwrap();
-    println!("Called prover");
+  let circ = Circuit {
+    N: N,
+    A: A,
+    B: B,
+    C: C,
+  };
 
-    assert!(MarlinInst::verify(&index_vk, &[c, d], &proof, rng).unwrap());
-    println!("Called verifier");
-    println!("\nShould not verify (i.e. verifier messages should print below):");
-    assert!(!MarlinInst::verify(&index_vk, &[a, a], &proof, rng).unwrap());
-  }
-}
+  let circ2 = Circuit {
+    N: N,
+    A: vec![None;N * N],
+    B: vec![None;N * N],
+    C: vec![None;N * N],
+  };
 
-fn main(){
-  let num_constraints = 100;
-  let num_variables = 25;
+  println!("Setting up");
+  let (index_pk, index_vk) = MarlinInst::index(&universal_srs, circ2).unwrap();
+  println!("Called index");
 
-  test_circuit(num_constraints, num_variables);
+  let time = std::time::Instant::now();
+  let proof = MarlinInst::prove(&index_pk, circ, rng).unwrap();
+  println!("Proved {:?}",time.elapsed());
 
-  let num_constraints = 26;
-  let num_variables = 25;
-
-  test_circuit(num_constraints, num_variables);
-
-  let num_constraints = 25;
-  let num_variables = 100;
-
-  test_circuit(num_constraints, num_variables);
-
-  let num_constraints = 25;
-  let num_variables = 26;
-
-  test_circuit(num_constraints, num_variables);
-
-  let num_constraints = 25;
-  let num_variables = 25;
-
-  test_circuit(num_constraints, num_variables);
+  assert!(MarlinInst::verify(&index_vk, &[Fr::one()], &proof, rng).unwrap());
+  println!("Called verifier");
 }
